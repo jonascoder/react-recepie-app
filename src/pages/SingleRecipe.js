7373b196510b24f56fa7ada768544ff30ac04b46
Jonas Rodon <jonascoder@gmail.com>
2019-04-01 21:49:13 +0100

    fix SingleRecipe

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-// import { recipeData } from "../data/tempDetails";
+import { recipeData } from "../data/tempDetails";
 import { Link } from "react-router-dom";
-export default class SingleRecipie extends Component {
+export default class SingleRecipe extends Component {
   constructor(props) {
     super(props);
     const id = this.props.match.params.id;
     this.state = {
+      // recipe: recipeData,
       recipe: {},
       id,
       loading: true
@@ -26,6 +27,7 @@ export default class SingleRecipie extends Component {
       console.log(error);
     }
   }
+
   render() {
     const {
       image_url,
@@ -37,7 +39,7 @@ export default class SingleRecipie extends Component {
     } = this.state.recipe;
     if (this.state.loading) {
       return (
-        <div>
+        <div className="container">
           <div className="row">
             <div className="col-10 mx-auto col-md-6 my-3">
               <h2 className="text-uppercase text-orange text-center">
@@ -54,7 +56,7 @@ export default class SingleRecipie extends Component {
           <div className="col-10 mx-auto col-md-6 my-3">
             <Link
               to="/recipes"
-              className="btn btn-warning mb-5 text-capitalize"
+              className="btn btn-warning mb-5 text-capatilize"
             >
               back to recipes list
             </Link>
@@ -66,37 +68,38 @@ export default class SingleRecipie extends Component {
             />
           </div>
           {/* info */}
-          <div className="col-10 mx-auto col-md-6 my-3" />
-          <h6 className="text-uppercase">{title}</h6>
-          <h6 className="text-warning text-capitalize text-slanted">
-            provided by {publisher}
-          </h6>
-          <a
-            href={publisher_url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-primary mt-2 text-capitalize"
-          >
-            Publisher webpage
-          </a>
-          <a
-            href={source_url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-success mt-2 mx-2 text-capitalize"
-          >
-            recipe url
-          </a>
-          <ul className="list-group mt-4">
-            <h2 className="mt-3 mb-4">ingredients</h2>
-            {ingredients.map((item, index) => {
-              return (
-                <li key={index} className="list-group-item text-slanted">
-                  {item}
-                </li>
-              );
-            })}
-          </ul>
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <h6 className="text-uppercase">{title}</h6>
+            <h6 className="text-warning text-capitalize text-slanted">
+              provided by {publisher}
+            </h6>
+            <a
+              href={publisher_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary mt-2 text-capitalize"
+            >
+              publisher webpage
+            </a>
+            <a
+              href={source_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-success mt-2 mx-2 text-capitalize"
+            >
+              recipe url
+            </a>
+            <ul className="list-group mt-4">
+              <h2 className="mt-3 mb-4">Ingredients</h2>
+              {ingredients.map((item, index) => {
+                return (
+                  <li key={index} className="list-group-item text-slanted">
+                    {item}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     );
